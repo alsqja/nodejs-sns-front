3fff0735e456762a023cdfbb8e804bacc0a45ea7
alsqja <alsqja2626@uno-solution.com>
2022-12-21 11:53:36 +0900

    change validation function when checkpass.length === 0

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,7 +1,9 @@
 export const checkPassValidation = (password: string, checkPass: string) => {
-  if (password !== checkPass) {
-    return "비밀번호가 일치하지 않습니다";
-  } else {
+  if (checkPass.length === 0) {
     return "";
   }
+  if (password !== checkPass) {
+    return "비밀번호가 일치하지 않습니다";
+  }
+  return "";
 };
